4ad5a8813ef371a83360fcabb64d0fec951f21f5
许飞 <feixu@tju.edu.cn>
2018-07-25 19:41:20 +0800

    优化样式

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,5 @@
 module PostsHelper
-  def content_preview(content, length: 500)
+  def content_preview(content, length: 300)
     preview = sanitize(content[0..length], tags: %w(br))
     content.length <= length ? preview : preview + '...'
   end
