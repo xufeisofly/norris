5c00c3998e9af28ac6c37db0ae866d3cc502cbf9
Norris <feixu@tju.edu.cn>
2018-11-13 13:33:07 +0800

    fix

--- app/models/games/blue_space_scene.rb
+++ app/models/games/blue_space_scene.rb
@@ -3,10 +3,6 @@
 class Games::BlueSpaceScene < ApplicationRecord
   has_many :conversations, class_name: 'Games::BlueSpaceConversation', dependent: :destroy, foreign_key: :scene_id
 
-  def current_conversation
-    Games::BlueSpaceConversation.find_by(id: current_conversation_id)
-  end
-
   def next(answer)
     scene_relation = Games::BlueSpaceSceneRelation.find_by(scene_id: id, answer: answer)
     self.class.find_by(id: scene_relation&.next_scene_id)
