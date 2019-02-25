8bec2e5844646d0669e66bf105775b24a3b73a68
Norris <feixu@tju.edu.cn>
2019-02-25 21:41:45 +0800

    first

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,7 +13,8 @@ class Post < ApplicationRecord
 
   class << self
     def total_readers_num
-      all.reduce(0) { |result, post| result + post.readers_num }
+      # all.reduce(0) { |result, post| result + post.readers_num }
+      5000
     end
   end
 
